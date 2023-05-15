ea8fcc28ad08450d52aacd58671eb842f53ae031
ashikmoosa <ashikmoosa96@gmail.com>
2023-05-15 23:28:53 +0530

    optimised pattern7

--- Section1 - Basics/Patterns/pattern7.java
+++ Section1 - Basics/Patterns/pattern7.java
@@ -1,3 +1,4 @@
+// BruteForce O(n^2)
 void printTriangle(int n) {
     // code here
     for(int i=0; i<n; i++) {
@@ -11,5 +12,19 @@ void printTriangle(int n) {
             System.out.print("*");
         }
         System.out.print("\n");
+    }
+}
+
+// Optimised O(n^2)
+void printTriangle(int n) {
+    // code here
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n - i; j++) {
+            System.out.print(" ");
+        }
+        for (int k = 1; k <= 2 * i - 1; k++) {
+            System.out.print("*");
+        }
+        System.out.println();
     }
 }
--- Section1 - Basics/Patterns/pattern7.js
+++ Section1 - Basics/Patterns/pattern7.js
@@ -1,3 +1,4 @@
+// BruteForce O(n^2)
 function printTriangle(n) {
 	let str = "";
 	for (let i = 0; i < n; i++) {
@@ -8,6 +9,23 @@ function printTriangle(n) {
 			str += "*";
 		}
 		for (let l = 0; l < i; l++) {
+			str += "*";
+		}
+		str += "\n";
+	}
+	console.log(str);
+}
+
+printTriangle(4);
+
+// Optimised O(n^2)
+function printTriangle(n) {
+	let str = "";
+	for (let i = 1; i <= n; i++) {
+		for (let j = 1; j <= n - i; j++) {
+			str += " ";
+		}
+		for (let k = 1; k <= 2 * i - 1; k++) {
 			str += "*";
 		}
 		str += "\n";
