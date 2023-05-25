bc83b6d2fca6237b30c5da5f72088afe116d5675
ashikmoosa <ashikmoosa96@gmail.com>
2023-05-25 12:58:21 +0530

    added cleaner code for pattern 7 & 8

--- Section1 - Basics/Patterns/pattern7.java
+++ Section1 - Basics/Patterns/pattern7.java
@@ -15,6 +15,22 @@ void printTriangle(int n) {
     }
 }
 
+// Striver 
+void printTriangle(int n) {
+    for(int i=0; i<n; i++) {
+        for(int j=0;j<n-i-1;j++) {
+            System.out.print(" ");
+        }
+        for(int k=0;k<2*i+1;k++) {
+            System.out.print("*");
+        }
+        for(int l=0;l<n-i-1;l++) {
+            System.out.print(" ");
+        }
+    System.out.print("\n");
+    }
+}
+
 // Optimised O(n^2)
 void printTriangle(int n) {
     // code here
--- Section1 - Basics/Patterns/pattern8.java
+++ Section1 - Basics/Patterns/pattern8.java
@@ -15,6 +15,23 @@ void printTriangle(int n) {
     }
 }
 
+// Striver
+void printTriangle(int n) {
+    // code here
+    for (int i=0; i<n; i++) {
+        for(int j=0; j<i; j++) {
+            System.out.print(" ");
+        }
+        for(int k=0; k<2*(n-i)-1; k++) {
+            System.out.print("*");
+        }
+        for(int l=0; l<i; l++) {
+            System.out.print(" ");
+        }
+    System.out.print("\n");
+    }
+}
+
 // Optimised O(n^2)
 void printTriangle(int n) {
     // code here
