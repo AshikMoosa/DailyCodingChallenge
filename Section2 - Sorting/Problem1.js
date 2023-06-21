cbdd766f005dc1b9a89890e6d93621fdaf4829cc
ashikmoosa <ashikmoosa96@gmail.com>
2023-06-21 11:38:04 +0530

    Added notes on optimisation

--- Section2 - Sorting/Problem1.js
+++ Section2 - Sorting/Problem1.js
@@ -1,7 +1,8 @@
+// Brute Force - O(N^2)
 function selectionSort(arr, n) {
 	for (let i = 0; i <= n - 2; i++) {
 		let selected = i;
-		for (let j = i; j <= n - 1; j++) {
+		for (let j = i + 1; j <= n - 1; j++) {
 			if (arr[j] < arr[selected]) {
 				selected = j;
 			}
@@ -15,3 +16,11 @@ function selectionSort(arr, n) {
 
 let result = selectionSort([4, 1, 3, 9, 7], 5);
 console.log(result);
+
+/*Notes
+In selection sort we find min element in each iteration and put it in its place.
+Also no need run i to last element, it will be sorted in final iteration automatically
+selected = j, updating index of min element
+then swapping it to its proper position.
+
+*/
--- Section2 - Sorting/Problem2.js
+++ Section2 - Sorting/Problem2.js
@@ -1,17 +1,22 @@
+// Brute Force O(N^2)
 function bubbleSort(arr, n) {
-	//code here
-	for (let i = 0; i < n; i++) {
-		for (let j = 0; j < n; j++) {
-			if (arr[i] < arr[j]) {
-				let temp = arr[i];
-				arr[i] = arr[j];
-				arr[j] = temp;
+	for (let i = 0; i <= n - 2; i++) {
+		for (let j = 0; j < n - i - 1; j++) {
+			if (arr[j] > arr[j + 1]) {
+				let temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
 			}
 		}
 	}
 	return arr;
 }
 
+let result = bubbleSort([4, 1, 3, 9, 7], 5);
+console.log(result);
+
 /*Notes
 Dont need to run inner loop n times last index will be sorted automatically
+Also, here max element will be in right position at each iterations(last pos)
+Hence j loop always start from 0 and doens't reach final position/ sorted positions
 */
