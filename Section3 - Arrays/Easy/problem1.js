a66a52a8e3459481cfe97486b573363e743cd26c
ashikmoosa <ashikmoosa96@gmail.com>
2023-07-15 18:00:18 +0530

    Added Array easy problem1

---
+++ Section3 - Arrays/Easy/problem1.js
@@ -0,0 +1,11 @@
+function largest(arr, n) {
+  // write your code here
+  let largest = arr[0];
+  for (let i = 0; i < n; i++) {
+    if (arr[i] > largest) largest = arr[i];
+  }
+  return largest;
+}
+
+let result = largest([1, 8, 7, 56, 90], 5);
+console.log(result);
