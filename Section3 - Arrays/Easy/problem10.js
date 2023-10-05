790d1b9bd22656c2e655b9e3a5a74499a42c146c
ashikmoosa <ashikmoosa96@gmail.com>
2023-10-05 12:08:51 +0530

    Added optimal for missing Number

--- Section3 - Arrays/Easy/problem10.js
+++ Section3 - Arrays/Easy/problem10.js
@@ -15,3 +15,41 @@ function missingNumber(nums) {
 
 let result = missingNumber([9, 6, 4, 2, 3, 5, 7, 0, 1]);
 console.log(result);
+
+// Better - O(N) - S(N)
+var missingNumber = function (nums) {
+  let n = nums.length;
+  let missingNum = -1;
+  const hashArr = new Array(n + 1).fill(0);
+  for (let i = 0; i < n; i++) {
+    hashArr[nums[i]] += 1;
+  }
+
+  for (let i = 0; i <= n; i++) {
+    if (hashArr[i] === 0) {
+      missingNum = i;
+    }
+  }
+  return missingNum;
+};
+
+// Optimal - O(N) - S(1)
+var missingNumber = function (nums) {
+  let n = nums.length;
+  let missingNum = -1;
+  let realSum = (n * (n + 1)) / 2;
+  let arrSum = 0;
+
+  for (let i = 0; i < n; i++) {
+    arrSum = arrSum + nums[i];
+  }
+
+  missingNum = realSum - arrSum;
+  return missingNum;
+};
+
+/*Notes
+In BF - we used sorting hence O(NLogN)
+In Better - we used hashMap hence O(N) with extra space of S(N)
+In Optimal - we use mathematical formula n(n+1)/2 for sum of n numbers to find missing number
+*/
